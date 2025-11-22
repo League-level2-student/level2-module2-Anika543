996b0f07bbf5b49ed05371564d797d0b4504e7f5
Aarit Jayaswal <aaritrj@gmail.com>
2025-11-22 11:19:08 -0800

    Mod 2 part 1 finish

--- src/_08_LeagueSnake/LeagueSnake.java
+++ src/_08_LeagueSnake/LeagueSnake.java
@@ -1,5 +1,7 @@
 package _08_LeagueSnake;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
 
 public class LeagueSnake extends PApplet {
@@ -11,10 +13,12 @@ public class LeagueSnake extends PApplet {
      * 
      * Put all the game variables here.
      */
-    Segment segment; 
+    Segment head; 
     int foodX;
     int foodY; 
-    
+    int direction = RIGHT; 
+    int score = 0; 
+    ArrayList<Segment> tailSegments = new ArrayList<Segment>();
     /*
      * Setup methods
      * 
@@ -22,16 +26,21 @@ public class LeagueSnake extends PApplet {
      */
     @Override
     public void settings() {
-        
+        setSize(500, 500); 
     }
 
     @Override
     public void setup() {
+        head = new Segment(100, 100); 
+        frameRate(30); 
+        dropFood(); 
         
     }
 
     void dropFood() {
         // Set the food in a new random location
+    	foodX = ((int)random(50)*10);
+        foodY = ((int)random(50)*10);
         
     }
 
@@ -43,21 +52,35 @@ public class LeagueSnake extends PApplet {
 
     @Override
     public void draw() {
-        
+        background(24, 24, 26);
+        drawFood();
+        move();
+        drawSnake(); 
+        eat(); 
     }
 
     void drawFood() {
         // Draw the food
+    	fill(89, 191, 52); 
+    	rect(foodX, foodY, 10, 10); 
         
     }
 
     void drawSnake() {
         // Draw the head of the snake followed by its tail
+    	fill(101, 103, 219);
+    	rect(head.x,head.y, 10, 10); 
+    	manageTail(); 
     }
 
     void drawTail() {
         // Draw each segment of the tail
-        
+    	for(int i = 0; i<tailSegments.size(); i++) {
+    		Segment temp = tailSegments.get(i); 
+    		fill(101, 103, 219);
+    		rect(temp.x, temp.y, 10, 10); 
+    	}
+    	
     }
 
     /*
@@ -70,11 +93,24 @@ public class LeagueSnake extends PApplet {
         // After drawing the tail, add a new segment at the "start" of the tail and
         // remove the one at the "end"
         // This produces the illusion of the snake tail moving.
-
+    	
+    	checkTailCollision(); 
+    	drawTail(); 
+    	Segment temp = new Segment(head.x, head.y) ;
+    	tailSegments.add(temp);
+    	tailSegments.remove(0); 
     }
 
     void checkTailCollision() {
         // If the snake crosses its own tail, shrink the tail back to one segment
+    	for(int i = 0; i<tailSegments.size(); i++) {
+    		if(head.x == tailSegments.get(i).x && head.y == tailSegments.get(i).y) {
+    			score=0; 
+    			tailSegments.clear(); 
+    			Segment temp = new Segment(head.x, head.y);
+    	    	tailSegments.add(temp);
+    		}
+    	}
         
     }
 
@@ -87,35 +123,71 @@ public class LeagueSnake extends PApplet {
     @Override
     public void keyPressed() {
         // Set the direction of the snake according to the arrow keys pressed
+    	if(key == CODED) {
+    	if(keyCode == UP && direction != DOWN) {
+    		direction = UP;
+    	}else if(keyCode==DOWN && direction != UP) {
+    		direction = DOWN;
+    		System.out.println("down");
+    	}else if (keyCode == LEFT && direction != RIGHT) {
+    		direction = LEFT;
+    	}else if (keyCode == RIGHT && direction != LEFT) {
+    		direction = RIGHT; 
+    	}
+    	}
+    	
+  
         
     }
 
     void move() {
         // Change the location of the Snake head based on the direction it is moving.
 
-        /*
+        
         if (direction == UP) {
             // Move head up
-            
+            head.y-=10; 
+            checkBoundaries(); 
         } else if (direction == DOWN) {
             // Move head down
-                
+        	head.y+=10; 
+        	checkBoundaries();    
         } else if (direction == LEFT) {
-            
+        	head.x-=10; 
+        	checkBoundaries(); 
         } else if (direction == RIGHT) {
-            
+        	head.x+=10; 
+        	checkBoundaries(); 
         }
-        */
+       
     }
 
     void checkBoundaries() {
         // If the snake leaves the frame, make it reappear on the other side
+    	if(head.y<0) {
+    		head.y=500; 
+    	}else if(head.y>500) {
+    		head.y=0;
+    	}else if(head.x>500) {
+    		head.x=0;
+    	}else if(head.x<0) {
+    		head.x=500; 
+    	}
         
     }
 
     void eat() {
         // When the snake eats the food, its tail should grow and more
+    	
         // food appear
+    	if(head.x==foodX&&head.y==foodY) {
+    		foodX = ((int)random(50)*10);
+            foodY = ((int)random(50)*10);
+    		score++; 
+    		Segment temp = new Segment(head.x, head.y);
+	    	tailSegments.add(temp);
+    		
+    	}
         
     }
 
